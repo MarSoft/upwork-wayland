8951e426c3c443b714ba61c22e65acaa6850618c
Semyon Maryasin <simeon@maryasin.name>
2020-02-12 02:38:38 +0300

    spit waybar-friendly status

--- screenshot_adapter.py
+++ screenshot_adapter.py
@@ -13,6 +13,7 @@
 
 import asyncio
 import datetime as dt
+import json
 import subprocess
 import sys
 
@@ -26,13 +27,16 @@ def debug(*msg):
 
 
 class ScreenshotInterface(ServiceInterface):
-    def __init__(self):
+    def __init__(self, on_shot=None):
         super().__init__('org.gnome.Shell.Screenshot')
+        self.on_shot = on_shot
 
     @method()
     def Screenshot(self, include_cursor: 'b', flash: 'b', filename: 's') -> 'bs':
-        debug('Got Screenshot call', include_cursor, flash, filename)
+        debug(dt.datetime.utcnow(), 'Got Screenshot call', include_cursor, flash, filename)
         subprocess.run(['grim', *(['-c'] if include_cursor else []), filename])
+        if self.on_shot:
+            self.on_shot()
         return [True, filename]
 
     @method()
@@ -50,9 +54,10 @@ class ScreenshotInterface(ServiceInterface):
 
 
 class IdleTime(ServiceInterface):
-    def __init__(self):
+    def __init__(self, on_shot):
         super().__init__('org.gnome.Mutter.IdleMonitor')
         self.last_active = dt.datetime.utcnow()
+        self.on_shot = on_shot
 
     async def start(self):
         try:
@@ -80,9 +85,71 @@ class IdleTime(ServiceInterface):
     def GetIdletime(self) -> 't':
         # What unit do we want?
         delta = dt.datetime.utcnow() - self.last_active
-        debug('Asked idletime. It is', delta)
+        debug(dt.datetime.utcnow(), 'Asked idletime. It is', delta)
+        if self.on_shot:
+            self.on_shot()
         # return milliseconds
         return round(delta.total_seconds() * 1000)
+
+
+class WaybarReporter:
+    def __init__(self):
+        self.last_shot = dt.datetime(2000, 1, 1)
+        self.last_idle = dt.datetime(2000, 1, 1)
+
+    def screenshot_taken(self):
+        self.last_shot = dt.datetime.utcnow()
+
+    def idle_taken(self):
+        self.last_idle = dt.datetime.utcnow()
+
+    async def report_waybar(self):
+        second = dt.timedelta(seconds=1)
+        minute = second * 60
+        interval = minute * 10
+        hour = minute * 60
+
+        while True:
+            now = dt.datetime.utcnow()
+            current_interval = dt.datetime.fromtimestamp(
+                now.timestamp() // 600 * 600)
+            next_interval = current_interval + interval
+            prev_interval = current_interval - interval
+            since_last = now - self.last_shot
+            till_next = next_interval - now
+
+            since_lastidle = now - self.last_idle
+            # When active, it will query idletime at least every minute.
+            # So if they didn't query it for a minute then they are inactive.
+            idle_active = since_lastidle < minute
+
+            # this interval has its screenshot taken already
+            this_taken = self.last_shot > current_interval
+            prev_taken = self.last_shot > prev_interval
+
+            percentage = 100 - till_next.total_seconds() / 600 * 100
+            if since_last > 24*hour:
+                since_last_fmt = 'inf'
+            else:
+                since_last_fmt = str(since_last // minute * minute)[:-3]
+                if since_last_fmt.startswith('0:'):
+                    since_last_fmt = since_last_fmt[2:]
+            till_next_fmt = str(till_next // second * second)[-4:]
+            cls = 'done' if this_taken else 'active' if idle_active else 'passive'
+
+            text = f'@{self.last_shot:%H:%M}  {since_last_fmt}  {till_next_fmt}'
+            #if not this_taken:
+            #    text += f'  {round(percentage, 1)}%'
+
+            print(json.dumps({
+                'text': text,
+                'class': cls,
+                'alt': cls,
+                'percentage': round(percentage),
+                'tooltip': f'{cls}',
+            }), flush=True)
+
+            await asyncio.sleep(1)
 
 
 async def main():
@@ -94,8 +161,12 @@ async def main():
         asyncio.get_event_loop().create_future(),
     ]
 
-    bus.export('/org/gnome/Shell/Screenshot', ScreenshotInterface())
-    idle = IdleTime()
+    reporter = WaybarReporter()
+    workers.append(reporter.report_waybar())
+
+    screenshots = ScreenshotInterface(on_shot=reporter.screenshot_taken)
+    bus.export('/org/gnome/Shell/Screenshot', screenshots)
+    idle = IdleTime(on_shot=reporter.idle_taken)
     await idle.start()
     if idle.worker:
         workers.append(idle.worker)
